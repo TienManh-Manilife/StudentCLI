c517593729d602a5b7a3d54472f71b5d3391d771
Nguyen Tien Manh <manilife217@gmail.com>
2026-01-07 00:20:57 +0700

    set lại môi trường ảo

--- main.py
+++ main.py
@@ -1,4 +1,29 @@
-from cli_menu import main
+# from cli_menu import main
+
+# if __name__ == "__main__":
+#     main()
+
+
+
+from textual.app import App, ComposeResult
+from textual.widgets import TabbedContent, TabPane, Static
+
+class StudentCLI(App):
+    def compose(self) -> ComposeResult:
+        
+        # Tạo container TabbedContent
+        with TabbedContent():
+            # Tab 1: Danh sách
+            with TabPane("Danh sách"):
+                yield Static("Danh sách sinh viên sẽ hiển thị ở đây")
+
+            # Tab 2: Thống kê
+            with TabPane("Thống kê"):
+                yield Static("Thống kê dữ liệu sẽ hiển thị ở đây")
+
+    def on_key(self, event): 
+        if event.key == "q": 
+            self.exit()
 
 if __name__ == "__main__":
-    main()
+    StudentCLI().run()
